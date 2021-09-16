13eef224e8050532ba8ce2a63e787e2413b26e1e
Olivier <olivier@ogasphere.com>
2021-09-16 18:49:18 +0200

    On repart de zero reinstallation skeleton

---
+++ meteo/src/Controller/WeatherController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WeatherController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('weather/index.html.twig', [
+            'controller_name' => 'WeatherController',
+        ]);
+    }
+}
