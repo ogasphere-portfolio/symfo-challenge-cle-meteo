bfa95f3d82461507e76c0f73dc678a4e98c2311b
Olivier <olivier@ogasphere.com>
2021-09-17 08:28:18 +0200

    dynamisation du widget

--- meteo/src/Controller/WeatherController.php
+++ meteo/src/Controller/WeatherController.php
@@ -23,4 +23,34 @@ class WeatherController extends AbstractController
             'weathers' => $weathers ,
         ]);
     }
+
+
+    /**
+     * @route("/weather/{id}", name="weather_widget", methods={"GET","HEAD"}, requirements={"id"="\d+"})"
+     * @param  mixed $id
+     * @return void
+     */
+    public function show($id, SessionInterface $session, WeatherModel $WeatherModel) 
+    {
+        
+        $weather = $WeatherModel->getWeatherByCityIndex($id);
+
+         
+        if ($weather === null) {
+            // affiche une page 404 avec le message d'erreur fournit en argument
+            throw $this->createNotFoundException('The oisal does not exist'); 
+          
+        }
+            // on ajoute la meteo en session
+            $session->set('last_weather', $weather);
+
+            $widget = $session->get('last_weather');
+           
+            return $this->render('_partials/sidebar.html.twig', [
+                'widget' => $widget,
+            ]);
+            
+           
+        
+    }
 }
