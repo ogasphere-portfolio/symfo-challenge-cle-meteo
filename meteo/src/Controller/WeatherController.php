97987006867d3e87bdd1a68321e1f36f0d12dceb
Olivier <olivier@ogasphere.com>
2021-09-17 07:19:56 +0200

    dynamisation de la page home

--- meteo/src/Controller/WeatherController.php
+++ meteo/src/Controller/WeatherController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\entity\WeatherModel;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class WeatherController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function list(SessionInterface $session,WeatherModel $WeatherModel ) : Response
     {
-        return $this->render('weather/index.html.twig', [
-            'controller_name' => 'WeatherController',
+       
+        $weathers = $WeatherModel->getWeatherData();
+       
+        return $this->render('weather/home.html.twig', [
+            'weathers' => $weathers ,
         ]);
     }
 }
